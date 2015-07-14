b9e9ad8fc98bf4dd030b5c0e4edf65a5abb730f6
Dylan R. E. Moonfire <d.moonfire@mfgames.com>
2015-07-13 23:30:10 -0500

    Working on populating stemmed words.

--- src/specs/tokens-spec.ts
+++ src/specs/tokens-spec.ts
@@ -6,24 +6,98 @@
 import types = require("node-author-intrusion");
 import plugin = require("../index");
 
-describe("tokens", function() {
-    it("splits a line into tokens", function() {
-        // Build up the arguments for the content.
-        var content = new types.Content();
-        content.lines.push(new types.Line(new types.Location(), "one two three"));
-        var args = new types.AnalysisArguments();
-        args.content = content;
+function setupLines(analysis?: any): types.Content {
+    // Build up the arguments for the content.
+    var content = new types.Content();
+    content.lines.push(
+        new types.Line(new types.Location("a", 1), "One Two Three."));
+    content.lines.push(
+        new types.Line(new types.Location("a", 2), "Four \"Five\" Six"));
+    content.lines.push(
+        new types.Line(new types.Location("a", 3), "Seven *Eight* Nine"));
 
-        // Process the plugin.
-        plugin.process(args);
+    // Populate the analysis options.
+    if (!analysis)
+    {
+        analysis = {};
+    }
 
-        // Verify the results.
-        expect(content.tokens.length).toEqual(3);
-        expect(content.tokens[0].text).toEqual("one");
-        expect(content.tokens[1].text).toEqual("two");
-        expect(content.tokens[2].text).toEqual("three");
+    // Process the plugin.
+    var args = new types.AnalysisArguments();
+    args.content = content;
+    args.analysis = analysis;
 
-        expect(content.lines.length).toEqual(1);
-        expect(content.lines[0].tokens.length).toEqual(3);
+    plugin.process(args);
+
+    // Return the content.
+    return content;
+}
+
+describe("default tokens", function() {
+    it("splits lines into tokens", function() {
+        var content = setupLines();
+
+        expect(content.tokens.length).toEqual(14);
+        expect(content.lines.length).toEqual(3);
+    });
+    it("splits line 0 into tokens", function() {
+        var content = setupLines();
+
+        expect(content.lines[0].tokens.length).toEqual(4);
+
+        expect(content.tokens[0].text).toEqual("One");
+        expect(content.tokens[0].normalized).toEqual("one");
+        expect(content.tokens[0].stem).toEqual(undefined);
+
+        expect(content.tokens[1].text).toEqual("Two");
+        expect(content.tokens[1].normalized).toEqual("two");
+        expect(content.tokens[1].stem).toEqual(undefined);
+
+        expect(content.tokens[2].text).toEqual("Three");
+        expect(content.tokens[2].normalized).toEqual("three");
+        expect(content.tokens[2].stem).toEqual(undefined);
+
+        expect(content.tokens[3].text).toEqual(".");
+        expect(content.tokens[3].normalized).toEqual(".");
+        expect(content.tokens[3].stem).toEqual(undefined);
+    })
+});
+
+describe("stemmed tokens", function() {
+    it("splits lines into tokens", function() {
+        var analysis = {
+            stemmer: "porter"
+        };
+        var content = setupLines(analysis);
+
+        expect(content.tokens.length).toEqual(14);
+        expect(content.lines.length).toEqual(3);
+    });
+    it("splits line 0 into tokens", function() {
+        var analysis = {
+            "name": "test",
+            "options": {
+                "stemmer": "porter"
+            }
+        };
+        var content = setupLines(analysis);
+
+        expect(content.lines[0].tokens.length).toEqual(4);
+
+        expect(content.tokens[0].text).toEqual("One");
+        expect(content.tokens[0].normalized).toEqual("one");
+        expect(content.tokens[0].stem).toEqual("on");
+
+        expect(content.tokens[1].text).toEqual("Two");
+        expect(content.tokens[1].normalized).toEqual("two");
+        expect(content.tokens[1].stem).toEqual("two");
+
+        expect(content.tokens[2].text).toEqual("Three");
+        expect(content.tokens[2].normalized).toEqual("three");
+        expect(content.tokens[2].stem).toEqual("three");
+
+        expect(content.tokens[3].text).toEqual(".");
+        expect(content.tokens[3].normalized).toEqual(".");
+        expect(content.tokens[3].stem).toEqual(".");
     })
 });
